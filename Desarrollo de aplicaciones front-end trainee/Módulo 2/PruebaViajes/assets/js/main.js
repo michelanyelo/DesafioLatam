382487bdb6c3e8f6ac1feb585a9ccd529c9a7901
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-07-01 13:04:03 -0400

    módulo 2 finalizando

--- Desarrollo de aplicaciones front-end trainee/Módulo 2/PruebaViajes/assets/js/main.js
+++ Desarrollo de aplicaciones front-end trainee/Módulo 2/PruebaViajes/assets/js/main.js
@@ -10,3 +10,25 @@ $(window).scroll(function () {
 });
 
 // smooth scroll
+
+// validación formulario de contacto
+(() => {
+    'use strict'
+    // obtengo todos los formularios a los que quiero aplicar estilos de validación personalizados de bootstrap
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // recorro cada uno de ellos y prevengo el envío
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            // si el formulario no es válido, prevengo el envío y la propagación del evento
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            // añado la clase 'was-validated' al formulario para aplicar los estilos de validación
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
+
